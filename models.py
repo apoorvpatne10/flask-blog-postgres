adfffe78400da82f2c70da1fa7c8e6f375255760
Apoorv Patne <apoorv.patne10@gmail.com>
2024-01-16 15:55:26 +0530

    minor updates : added comments

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 
 class User(db.Model):
+    # User model for storing user information
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(512), nullable=False)
@@ -10,6 +11,7 @@ class User(db.Model):
 
 
 class Blog(db.Model):
+    # Blog model for storing blog post information
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
@@ -18,4 +20,5 @@ class Blog(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     def __repr__(self):
+        # A representation of the Blog object for debugging purposes
         return f"BlogPost(id={self.id}, title={self.title})"
